58ce6eb2135eebdf592491bb581bf5a32de5eab4
khanh.luong <baokhanh2710cntt@gmail.com>
2021-07-18 13:36:18 +1200

    add func remove contact.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { uuid } from 'uuidv4'
 import './App.css';
 import Header from './Header'
 import AddContact from './AddContact'
@@ -7,19 +8,37 @@ import ContactList from './ContactList'
 
 function App() {
 
+  const LOCAL_STORAGE_KEY = "contacts"
 
   const [contacts, setContacts] = useState([])
 
   const addContactHandler = (contact) => {
     console.log(contact)
-    setContacts([...contacts, contact])
+    setContacts([...contacts, { id: uuid(), ...contact }])
   }
+
+  const removeContactHandle = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    })
+
+    setContacts(newContactList)
+  }
+
+  useEffect(() => {
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (retriveContacts) setContacts(retriveContacts);
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts))
+  }, [contacts])
 
   return (
     <div className="ui container">
       <Header />
       <AddContact addContactHandler={addContactHandler} />
-      <ContactList contacts={contacts} />
+      <ContactList contacts={contacts} getContactId={removeContactHandle} />
     </div>
 
   );
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -3,9 +3,12 @@ import CardContact from "./ContactCard"
 
 const ContactList = (props) => {
     console.log(props)
+    const deleteConactHandler = (id) => {
+        props.getContactId(id);
+    };
     const renderContactList = props.contacts.map((contact) => {
         return (
-            <CardContact contact={contact}></CardContact>
+            <CardContact contact={contact} clickHandler={deleteConactHandler}></CardContact>
         )
     });
     return <div className="ui celled list">
